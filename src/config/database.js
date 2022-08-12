bbc744570fc0f01c457a9e4412dfc560152e56e7
José <zeca.j.a@outlook.com>
2022-08-12 09:53:46 -0300

    dispensa

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     dialect: 'mysql',
     host: 'localhost',
     username: 'root',
-    password: '$Le021427',
+    password: 'admin',
     database: 'vgrow-app-database',
     define: {
         timestamps: true,
--- src/database/migrations/20220722002320-create-users.js
+++ src/database/migrations/20220722002320-create-users.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('user', { 
+    await queryInterface.createTable('users', { 
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
@@ -41,6 +41,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('user');
+    await queryInterface.dropTable('users');
   }
 };
